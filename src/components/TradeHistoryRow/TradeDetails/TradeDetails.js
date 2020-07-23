3d04dfb7d59c1f6d56a7acedad7dabd53edf111a
JamesWattersIV <53174355+JamesWattersIV@users.noreply.github.com>
2020-07-23 18:21:29 +0200

    Add flag API to trade history table

--- src/components/TradeHistoryRow/TradeDetails/TradeDetails.js
+++ src/components/TradeHistoryRow/TradeDetails/TradeDetails.js
@@ -3,10 +3,16 @@ import React from "react";
 //SCSS Import
 import "./TradeDetails.scss";
 
+//API for Flag
+
 const TradeDetails = ({ currency, amount }) => {
   const formatter = new Intl.NumberFormat("fr");
+  const BASE_URL = "https://www.countryflags.io/";
+  const flag = BASE_URL + currency.substring(0, 2) + "/flat/64.png";
+
   return (
     <div className="date-col">
+      <img className="flag" src={flag} alt="" />
       <h3 className="currency">{currency}</h3>
       <h3>{formatter.format(amount)}</h3>
     </div>
