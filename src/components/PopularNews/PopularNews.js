a582bc3490da818ff3bbbfa5e3e5146dc5b9e94c
JamesWattersIV <53174355+JamesWattersIV@users.noreply.github.com>
2020-07-24 13:54:56 +0200

    change NEWS API source

--- src/components/PopularNews/PopularNews.js
+++ src/components/PopularNews/PopularNews.js
@@ -11,14 +11,17 @@ const PopularNews = ({
   url,
   urlToImage,
 }) => {
+  const NullURL =
+    "https://pbs.twimg.com/profile_images/761160432451252224/j5fy1mDI_400x400.jpg";
+
   return (
     <a href={url} target="_blank" rel="noopener noreferrer">
       <div className="news-article-pop">
-        <img src={urlToImage} alt="" />
+        <img src={!urlToImage ? NullURL : urlToImage} alt="" />
         <div className="info-container">
           <h1>{title}</h1>
           <h2>{author}</h2>
-          <h3>{publishedAt.split("T")[0]}</h3>
+          <h3>{publishedAt.split(" ")[0]}</h3>
           <p>{description}</p>
         </div>
       </div>
--- src/pages/News/News.js
+++ src/pages/News/News.js
@@ -11,12 +11,13 @@ import "./News.scss";
 const News = () => {
   //News API End point
   const BASE_URL =
-    "https://newsapi.org/v2/everything?q=forex&apiKey=bfc255afbf99427c8d41a9fbb294afc5";
+    "https://gnews.io/api/v3/search?q=forex&token=430a5cd7eda10574a972151c5673d9c9&country=uk";
 
   const sortBy = "publishedAt";
   const language = "en";
   const [loading, setLoading] = useState(false);
   const [articles, setArticles] = useState([]);
+  const [uniqueArray, setUniqueArray] = useState([]);
 
   //Fetch News Articles when page is loaded
   useEffect(() => {
@@ -24,10 +25,15 @@ const News = () => {
     fetch(BASE_URL + "&sortBy=" + sortBy + "&language=" + language)
       .then((res) => res.json())
       .then((data) => {
-        setArticles(data.articles.slice(0, 5));
+        setArticles(data.articles.filter(onlyUnique));
         setLoading(false);
       });
   }, []);
+
+  //Remove duplicate articles
+  const onlyUnique = (value, index, self) => {
+    return self.indexOf(value) === index;
+  };
 
   return (
     <Fragment>
@@ -42,13 +48,13 @@ const News = () => {
         <div className="divider"></div>
         {articles.map((article) => (
           <PopularNews
-            key={article.title}
-            author={article.author}
+            key={article.title + article.source.name}
+            author={article.source.name}
             description={article.description}
             publishedAt={article.publishedAt}
             title={article.title}
             url={article.url}
-            urlToImage={article.urlToImage}
+            urlToImage={article.image}
           />
         ))}
       </div>
